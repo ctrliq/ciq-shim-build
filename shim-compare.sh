e1eb3df0f7085c91c617d51ce4b23801b8cba3a7
Jason Rodriguez <jrodriguez@ciq.com>
2024-05-17 14:44:23 -0400

    removing ia32 from compare script

--- shim-compare.sh
+++ shim-compare.sh
@@ -8,16 +8,12 @@ Shim Comparison, original binary vs. freshly built binaries:
 
 SHA256 sums ::
 $(sha256sum /shimx64.efi   /shim_result/usr/share/shim/*/x64/shim*.efi)
-$(sha256sum /shimia32.efi   /shim_result/usr/share/shim/*/ia32/shim*.efi)
 
 Binary compare (blank output means binaries are the same) ::
 $(cmp /shim_result/usr/share/shim/*/x64/shimx64.efi /shimx64.efi)
-$(cmp /shim_result/usr/share/shim/*/ia32/shimia32.efi /shimia32.efi)
 
 
 Pesign checks ::
-$(pesign -h -P -i /shim_result/usr/share/shim/*/ia32/shimia32.efi)
-$(pesign -h -P -i /shimia32.efi)
 $(pesign -h -P -i /shim_result/usr/share/shim/*/x64/shimx64.efi)
 $(pesign -h -P -i /shimx64.efi)
 "
